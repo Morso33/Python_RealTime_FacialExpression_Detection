1fd6e67c0e0f5aa1e3f8fe3df2ae320ccdb00dab
Morso <tom.laukkanen@icloud.com>
2023-04-12 10:54:53 +0300

    made code, you do the rest.

--- main.py
+++ main.py
@@ -2,6 +2,50 @@ import cv2, os
 import numpy as np
 from PIL import Image
 
+faceTypes = ["glasses", "happy", "leftlight", "noglasses", "normal", "rightlight", "sad", "sleepy", "surprised", "wink"]
+face_images = []
+face_labels = []
+
+
+def get_subjects_length():
+    return 18
+
+def create_model():
+    global face_images, face_labels
+    for i in range(0, get_subjects_length()):
+        subject = i
+        if (subject < 10):
+            subject = "0" + str(subject)
+
+        for j in range (0, len(faceTypes)):
+            #Get the file name
+            filename = "subject" + str(subject) + "." + str(faceTypes[j])
+            #Does the file exist?
+            if not os.path.isfile("faces/" + filename):
+                continue
+            print ("Loaded " + filename)
+            # Load the image
+            image = cv2.imread("faces/" + filename, cv2.IMREAD_GRAYSCALE)
+            #Did it load correctly?
+            if image is None:
+                print("Error loading image: " + filename)
+                continue
+            image = cv2.resize(image, (100, 100))
+            face_images.append(image)
+            face_labels.append(j)
+
+    print("Creating model...")    
+    # Create the LBPH model
+    face_images = np.array(face_images)
+    face_labels = np.array(face_labels)
+    model = cv2.face.LBPHFaceRecognizer_create()
+    # Train the model on the face images and labels
+    model.train(face_images, face_labels)
+    print("Model created!")
+    return model
+
+model = create_model()
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #Setup webcam
 print("starting webcam, please wait...")
 cap = cv2.VideoCapture(0)
@@ -11,12 +55,32 @@ if not cap.isOpened():
 # Loop until 'esc' is pressed
 while True:
     ret, frame = cap.read()
+    #Get the FPS
     fps = cap.get(cv2.CAP_PROP_FPS)
     cv2.putText(frame, "FPS: " + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    #Get the gpu usage
+    gpu_usage = cv2.cuda.getCudaEnabledDeviceCount()
+    cv2.putText(frame, "GPU: " + str(gpu_usage), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     #Get the webcam frame and convert it to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Frame', gray)
 
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    # Loop over each detected face
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = cv2.resize(roi_gray, (100, 100))
+        label, confidence = model.predict(roi_gray)
+
+        # Display the emotion label on the frame
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        label_text = "Emotion: " + str(faceTypes[label])
+        cv2.putText(gray, label_text, (x, y-10), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+        # Draw a rectangle around the face
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('Frame', gray)
     if cv2.waitKey(1) == 27: #ESC
         break
 # When everything done, release the capture
